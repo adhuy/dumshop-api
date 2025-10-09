caef43a81bd0d15aa15ac0eb30f0d58510fbc84d
Adhuy <yudhamerdeka91@gmail.com>
2025-10-09 17:20:11 +0700

    Add CORS in app.js

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 import express from "express";
 import userRoutes from "./routes/users.js";
+import cors from "cors";
 
 const app = express();
 
 // Middleware untuk membaca JSON
 app.use(express.json());
+
+// CORS
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://<NEXT_FRONTEND_DOMAIN>"],
+    credentials: true, // for COOKIE
+  })
+);
 
 // Routing
 app.use("/api/users", userRoutes);
